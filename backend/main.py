70af977be09a068dc52efa4d918ad9bb4a0f2f3c
Sumit Kaushik <sumitkaushik81@gmail.com>
2026-05-28 18:33:58 +0530

    Use live backend API

--- backend/main.py
+++ backend/main.py
@@ -23,7 +23,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "http://127.0.0.1:3000"
+        "http://127.0.0.1:3000",
+        "https://shrivinayaka-ai.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
